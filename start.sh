54ac760443bfe1d5d59d6934c69e2f6a6448b438
baileybb24 <baileybb24@icloud.com>
2026-03-23 17:01:50 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 echo "Starting stream server..."
 python stream.py &
 
+sleep 2
+
 echo "Starting Highrise bot..."
-python -m highrise bot:StarlightSnugglesBot $ROOM_ID $API_TOKEN
+
+python -m highrise bot:StarlightSnugglesBot $ROOM_ID $API_TOKEN || echo "BOT CRASHED"
